923b08a5e65d6ba11e3300b2eaed5e02bec5f92d
thientrankhh <thientrankhh@gmail.com>
2023-09-04 23:56:57 +0700

    feat: update set user agent in google chrome

--- proxy-changer/Form1.Designer.cs
+++ proxy-changer/Form1.Designer.cs
@@ -102,6 +102,8 @@
             this.txtIp = new System.Windows.Forms.TextBox();
             this.txtPort = new System.Windows.Forms.TextBox();
             this.btnReset = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.serverMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -556,11 +558,30 @@
             this.btnReset.UseVisualStyleBackColor = true;
             this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(644, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 39);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Change";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(644, 161);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 26);
+            this.textBox1.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1062, 550);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.txtPort);
             this.Controls.Add(this.txtIp);
@@ -653,6 +674,8 @@
         private System.Windows.Forms.TextBox txtIp;
         private System.Windows.Forms.TextBox txtPort;
         private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
